b589590963d0dc5f2bb667b5c133f92dd6e46112
daniitoro29 <daniitoro29@gmail.com>
2023-10-06 23:34:07 -0500

    Add process

--- src/routes/procesosRouter.js
+++ src/routes/procesosRouter.js
@@ -1,6 +1,14 @@
 const { Router } = require('express');
 
 const procesosRouter = Router();
+const { getProcessHandler,
+    createProcessHandler,
+    deleteProcessHandler,
+    updateProcessHandler } = require('../handlers/processHandlers');
 
+procesosRouter.post('/', createProcessHandler);
+procesosRouter.get('/', getProcessHandler);
+procesosRouter.delete('/:id', deleteProcessHandler);
+procesosRouter.put('/:id', updateProcessHandler);
 
 module.exports = procesosRouter;
